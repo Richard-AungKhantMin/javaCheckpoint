363f7121ee6e672fca6b17e6dc10fab708658ccd
amin <aungkhantmin.dev@gmail.com>
2026-04-19 23:06:45 +0300

    Refactor DoubleLinkedList and SingleLinkedList methods for improved index access and element manipulation

--- gp3/SingleLinkedList/SingleLinkedList.java
+++ gp3/SingleLinkedList/SingleLinkedList.java
@@ -1,4 +1,5 @@
 package gp3.SingleLinkedList;
+
 public class SingleLinkedList implements LinkedList {
     private Node head;
     private int size;
@@ -11,80 +12,67 @@ public class SingleLinkedList implements LinkedList {
             this.value = value;
             this.next = null;
         }
-        public void setNext(Node next) {
-            this.next = next;
-        }
     }
 
     @Override
     public int at(int index) {
-        if (index <0 || index > this.size) {
-            return -1;
-        }
-        if (index == 0) {
-            return this.head.value;
-        }
-        if (index == this.size ) {
-            return -1;
-        }
-        Node currentNode = this.head;
-        for (int i = 0; i < index; i++) {
-             currentNode = this.next(currentNode);
-        }
-        return currentNode.value;
+        // Implementation for accessing an element by its index
+        
+        if (index < 0 || index >= size) return -1;
+        
+        return jump(index).value;
     }
 
     @Override
     public void add(int value) {
-        if (head == null) {
-            size = 0;
-            Node last = null;
-            this.head = new Node(value);
-            this.head.setNext(last);
-        } else {
-            Node currentNode =  this.head;
-            while (currentNode.next != null) {
-                currentNode = this.next(currentNode);
-            }
-            Node newNode = new Node(value);
-            newNode.setNext(currentNode.next);
-            currentNode.setNext(newNode);
+        // Implementation for adding an element at the end of the list
+          Node newNode = new Node(value);
+        
+        if (size == 0){
+            this.head = newNode;
+        }else{
+            Node tail = jump(size-1);
+            tail.next = newNode;
         }
-        this.size++;
+        
+        size++;
+        
     }
 
     @Override
     public void remove(int index) {
-        if (index <0 || index > this.size) {
-            return;
-        }
-        if (index == 0) {
+        // Implementation for removing an element by its index
+        if (index < 0 || index >= size) return;
+        
+          if (index == 0){
             this.head = this.head.next;
-            size--;
-            return;
+        }else{
+            Node d = jump(index-1);
+            d.next = this.next(d.next);
         }
-        Node currentNode = this.head;
-        for (int i = 0; i < index -1; i++) {
-//            if ( index == 2) {
-//                System.out.println("currentNode " + currentNode.value);
-//                System.out.println("current Step " + i);
-//            }
-
-           currentNode = this.next(currentNode);
-        }
-
-        currentNode.next = this.next(currentNode.next);
+        
+        
+        
         size--;
     }
 
     @Override
     public int size() {
+        // Implementation for accessing an element by its index
         return this.size;
-         // Implementation for accessing an element by its index
     }
 
     private Node next(Node node) {
+        // Print the message "Go to next node"
         System.out.println("Go to next node");
         return node.next;
+    }
+    
+    private Node jump(int index){
+        Node current = this.head;
+        for (int i = 0; i < index; i++){
+            current = this.next(current);
+        }
+        return current;
     }
 }
